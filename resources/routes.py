56e52b1c70740ec0164d386c41b72ea849d29ec7
NotyCode <josti-olaya@upc.edu.co>
2023-10-17 15:48:22 -0500

    avance

--- resources/routes.py
+++ resources/routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from resources import user
 from resources import auth
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
+api_router.include_router(user.router)
